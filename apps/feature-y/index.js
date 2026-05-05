97f71fbf21bb455d54f72e6137169304c974c49b
BrookGT <birukgebisa7@gmail.com>
2026-05-05 13:52:31 +0300

    Initial monorepo setup

---
+++ apps/feature-y/index.js
@@ -0,0 +1,6 @@
+import { Button } from "@fittrack/ui-components";
+import { testUtil } from "@fittrack/utils";
+
+console.log("Feature Y");
+console.log(Button());
+console.log(testUtil());
---
+++ node_modules/feature-x/index.js
@@ -0,0 +1,5 @@
+import { Button } from "@fittrack/ui-components";
+import { testUtil } from "@fittrack/utils";
+
+console.log(Button());
+console.log(testUtil());
